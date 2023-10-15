0ffdc11386af8fd5e201735d50ca57c92d78c7cd
G Martin <georgemartin1603@yahoo.com>
2023-10-14 20:40:01 -0500

    new user category and item setup working

--- functions/helpers/firebase.js
+++ functions/helpers/firebase.js
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../private/farm-report-86ac2-firebase-adminsdk-iojy6-b17a0bea38.json");
+// var serviceAccount = require("../private/farm-report-86ac2-firebase-adminsdk-iojy6-b17a0bea38.json");
+var serviceAccount = require("../private/farm-report-staging-firebase-adminsdk-8vdx7-fbdbd4e199.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- functions/index.js
+++ functions/index.js
@@ -1,228 +1,259 @@
 const functions = require("firebase-functions");
 const { db } = require("./helpers/firebase");
-const dotenv = require("dotenv");
-const fs = require("fs");
+require("dotenv").config();
 
-dotenv.config();
 
-const dev = process.env.FUNCTIONS_EMULATOR;
-
-let origin = dev
-  ? "http://localhost:3000"
-  : "https://farm-report-next.vercel.app";
-
-// exports.copyToLocal = functions.https.onRequest(async (req, res) => {
+// exports.writeData = functions.https.onRequest(async (req, res) => {
 //   res.set("Access-Control-Allow-Origin", origin);
 //   const body = JSON.parse(req.body);
-//   const LIMIT = 200;
-//   await db
-//     .collection(body.coll)
-//     .limit(LIMIT)
-//     .get()
-//     .then((docSnap) => {
-//       if (docSnap.empty) {
-//         console.log("No matching documents.");
-//         return res
-//           .json(JSON.stringify({ message: "No matching documents." }))
-//           .send();
-//       } else if (docSnap.size === LIMIT) {
-//         console.log("Query limit reached.");
-//         return res
-//           .json(JSON.stringify({ message: "Query limit reached." }))
-//           .send();
-//       } else {
-//         docSnap.forEach((doc) => {
-//           let data = doc.data();
-//           fs.writeFile(
-//             `C:\/Users\/georg\/Documents\/data\/${body.coll}/${doc.id}.json`,
-//             JSON.stringify(data),
-//             (err) => {
-//               if (err) {
-//                 console.log(err);
-//               }
-//             }
-//           );
-//         });
-//         return res
-//           .json(
-//             JSON.stringify({
-//               message: `"Success", ${docSnap.size} documents retrieved`,
-//             })
-//           )
-//           .send();
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.json(JSON.stringify(error)).send();
-//     });
+//   db.collection(body.coll)
+//     .doc("profile")
+//     .set({ names: body.data }, { merge: true });
+
+//   res.json(JSON.stringify({ message: "Success" })).send();
 // });
 
-// exports.copyToFirebase = functions.https.onRequest(async (req, res) => {
+// exports.saveReport = functions.https.onRequest(async (req, res) => {
 //   res.set("Access-Control-Allow-Origin", origin);
 //   const body = JSON.parse(req.body);
-//   fs.readdir(
-//     `C:\/Users\/georg\/Documents\/data\/${"expenses"}`,
-//     (err, docs) => {
-//       if (err) {
-//         console.log(err);
-//         return res
-//           .json(JSON.stringify({ message: "Error reading local folder" }))
-//           .send();
-//       } else {
-//         docs.forEach((doc) => {
-//           fs.readFile(
-//             `C:\/Users\/georg\/Documents\/data\/${"expenses"}/${doc}`,
-//             async (err, data) => {
-//               if (err) {
-//                 console.log(err);
-//                 return res
-//                   .json(
-//                     JSON.stringify({ message: "Error reading local documents" })
-//                   )
-//                   .send();
-//               } else {
-//                 let obj = JSON.parse(data);
-//                 // let name = obj.landLord;
-//                 // let commodity = obj.crop;
-//                 // let year = obj.year;
-//                 // let id = `${name}-${commodity}-${year}`;
-//                 // obj.id = id;
-//                 // obj.name = name;
-//                 // obj.commodity = commodity;
-//                 await db
-//                   .collection(body.coll)
-//                   .doc("profile")
-//                   .collection("expenses")
-//                   .doc(obj.id)
-//                   .set(obj, { merge: true })
-//                   .catch((error) => {
-//                     console.log(error);
-//                   });
-//               }
-//             }
-//           );
-//         });
-//       }
-//       return res.json(JSON.stringify({ message: "Success" })).send();
-//     }
-//   );
-// });
-
-// exports.convertData = functions.https.onRequest(async (req, res) => {
-//   res.set("Access-Control-Allow-Origin", origin);
-//   const body = JSON.parse(req.body);
-//   const LIMIT = 200;
-//   let deleted = 0;
+//   console.log(body);
 //   await db
 //     .collection(body.coll)
 //     .doc("profile")
 //     .collection("reports")
-//     .limit(LIMIT)
-//     .get()
-//     .then((docSnap) => {
-//       if (docSnap.empty) {
-//         console.log("No matching documents.");
-//         return res
-//           .json(JSON.stringify({ message: "No matching documents." }))
-//           .send();
-//       } else if (docSnap.size === LIMIT) {
-//         console.log("Query limit reached.");
-//         return res
-//           .json(JSON.stringify({ message: "Query limit reached." }))
-//           .send();
-//       } else {
-//         docSnap.forEach(async (doc) => {
-//           // replace landLord with name and crop with comodity
-//           // new report id = `${name}-${comodity}-${year}`
-//           let data = doc.data();
-//           let name = data.landLord;
-//           let comodity = data.crop;
-//           let year = data.year;
-//           let id = `${name}-${comodity}-${year}`;
-//           let report = {
-//             id: id,
-//             name: name,
-//             comodity: comodity,
-//             year: year,
-//             ...data,
-//           };
-//           await db
-//             .collection(body.coll)
-//             .doc("profile")
-//             .collection("reports")
-//             .doc(report.id)
-//             .set(report, { merge: true })
-//             .catch((error) => {
-//               console.log(error);
-//             });
-//         });
-//         return res
-//           .json(
-//             JSON.stringify({
-//               message: `"Success", ${deleted} documents deleted`,
-//             })
-//           )
-//           .send();
+//     .doc(body.report.id)
+//     .set(body.report, { merge: true })
+//     .then(async () => {
+//       if (body.profile) {
+//         await db
+//           .collection(body.coll)
+//           .doc("profile")
+//           .set(body.profile, { merge: true });
 //       }
+//       return res
+//         .json(JSON.stringify({ message: "Success", status: "success" }))
+//         .send();
 //     })
 //     .catch((error) => {
 //       console.log(error);
-//       res.json(JSON.stringify(error)).send();
+//       return res.json(JSON.stringify(error)).send();
 //     });
 // });
 
-exports.writeData = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", origin);
-  const body = JSON.parse(req.body);
-  db.collection(body.coll)
-    .doc("profile")
-    .set({ names: body.data }, { merge: true });
+// exports.saveItem = functions.https.onRequest(async (req, res) => {
+//   res.set("Access-Control-Allow-Origin", origin);
+//   const body = JSON.parse(req.body);
+//   await db
+//     .collection(body.coll)
+//     .doc("profile")
+//     .collection("expenses")
+//     .doc(body.item.id)
+//     .set(body.item, { merge: true })
+//     .then(() => {
+//       return res.json(JSON.stringify({ message: "Success" })).send();
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       return res.json(JSON.stringify(error)).send();
+//     });
+// });
 
-  res.json(JSON.stringify({ message: "Success" })).send();
+// rewrite the above as an express app with CORS enabled
+const express = require("express");
+const cors = require("cors");
+const moment = require('moment');
+const app = express();
+
+const handleResponse = (res, statusObj, load) => {
+  const { code, status, message } = statusObj;
+  let resObj = {
+    status: status,
+    message: message,
+    data: load,
+  };
+  if (!status) console.error(load)
+  return res.status(code).json(resObj).send();
+}
+
+app.post("/addLog", async (req, res) => {
+  try {
+    let file = new Date().toDateString();
+    let logEntry = moment().format('HH:mm:ss');
+    console.log("addLog", file);
+    console.log(req.body);
+    const body = req.body;
+    let log = body.data;
+    await db
+      .collection("LOGS")
+      .doc(file)
+      .set({[logEntry]: log}, { merge: true })
+      .then(async () => {
+        let statusObj = {code:200, status: true, message: "Success" };
+        let payload = log;
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
 });
 
-exports.saveReport = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", origin);
-  const body = JSON.parse(req.body);
-  console.log(body);
-  await db
-    .collection(body.coll)
-    .doc("profile")
-    .collection("reports")
-    .doc(body.report.id)
-    .set(body.report, { merge: true })
-    .then(async () => {
-      if (body.profile) {
-        await db
-          .collection(body.coll)
-          .doc("profile")
-          .set(body.profile, { merge: true });
-      }
-      return res
-        .json(JSON.stringify({ message: "Success", status: "success" }))
-        .send();
-    })
-    .catch((error) => {
-      console.log(error);
-      return res.json(JSON.stringify(error)).send();
-    });
+app.post("/getReport", async (req, res) => {
+  console.log("getReport");
+  try {
+    const body = req.body;
+    let report = body.data;
+    await db
+      .collection(body.coll)
+      .doc("profile")
+      .collection("reports")
+      .doc(report)
+      .get()
+      .then(async (doc) => {
+        let statusObj = {code:200, status: true, message: "Success" };
+        let payload = doc.data();
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
 });
 
-exports.saveItem = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", origin);
-  const body = JSON.parse(req.body);
-  await db
-    .collection(body.coll)
-    .doc("profile")
-    .collection("expenses")
-    .doc(body.item.id)
-    .set(body.item, { merge: true })
-    .then(() => {
-      return res.json(JSON.stringify({ message: "Success" })).send();
-    })
-    .catch((error) => {
-      console.log(error);
-      return res.json(JSON.stringify(error)).send();
-    });
+app.post("/createReport", async (req, res) => {
+  console.log("createReport");
+  try {
+    const body = req.body;
+    let report = body.data;
+    await db
+      .collection(body.coll)
+      .doc("profile")
+      .collection("reports")
+      .doc(report.id)
+      .set(report, { merge: true })
+      .then(async () => {
+        // if (body.profile) {
+        //   await db
+        //     .collection(body.coll)
+        //     .doc("profile")
+        //     .set(body.profile, { merge: true });
+        // }
+        let statusObj = {code:200, status: true, message: "Success" };
+        let payload = report;
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
 });
+
+app.post("/writeData", async (req, res) => {
+  console.log("writeData", req.body);
+  try {
+    const body = req.body;
+    let user = body.user;
+    let doc = body.coll;
+    let data = body.data;
+    await db
+      .collection(user)
+      .doc(doc)
+      .set({ "values": data }, { merge: true })
+      .then(async () => {
+        let statusObj = {code:200, status: true, message: "Success" };
+        let payload = data;
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
+});
+
+app.post("/createCategory", async (req, res) => {
+  console.log("createCategory", req.body);
+  try {
+    const body = req.body;
+    let user = body.user;
+    let category = body.data;
+    await db
+      .collection(user)
+      .doc("profile")
+      .collection("expenses")
+      .doc(category.id)
+      .set(category)
+      .then(async () => {
+        let statusObj = {code:200, status: true, message: "Success" };
+        let payload = category;
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
+});
+
+app.post("/saveReport", async (req, res) => {
+  try {
+    const body = req.body;
+    console.log(body);
+    await db
+      .collection(body.coll)
+      .doc("profile")
+      .collection("reports")
+      .doc(body.report.id)
+      .set(body.report, { merge: true })
+      .then(async () => {
+        if (body.profile) {
+          await db
+            .collection(body.coll)
+            .doc("profile")
+            .set(body.profile, { merge: true });
+        }
+        let statusObj = {code:200, status: true, message: "Report Saved" };
+        let payload = {};
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
+});
+
+app.post("/saveItem", async (req, res) => {
+  console.log("saveItem", req.body);
+  try {
+    const body = req.body;
+    await db
+      .collection(body.user)
+      .doc("profile")
+      .collection("expenses")
+      .doc(body.data.id)
+      .set(body.data, { merge: true })
+      .then(() => {
+        let statusObj = {code:200, status: true, message: "Item Saved" };
+        let payload = {};
+        handleResponse(res, statusObj, payload);
+      })
+  } catch (error) {
+    let statusObj = {code: error.code? error.code : 500, status: false, message: "ERROR" };
+    console.log(statusObj);
+    handleResponse(res, statusObj, error);
+  }
+});
+
+// apply CORS to all routes
+const corsHandler = cors({ origin: true });
+const applyCORS = (handler) => (req, res) => {
+  return corsHandler(req, res, (_) => {
+      return handler(req, res);
+  });
+};
+
+exports.app = functions.https.onRequest(applyCORS(app));
